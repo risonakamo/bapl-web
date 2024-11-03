30deb3aa618c45c5d08e2c68cd6f7654cb70ed24
risonakamo <hmykanae@gmail.com>
2024-11-03 17:34:25 -0500

    experimenting with video element

--- web/pages/index/index.svelte
+++ web/pages/index/index.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+import {onMount} from "svelte";
 
+import Bapple from "@/assets/bapple.mp4";
+
+onMount(()=>{
+    videoElement.click();
+    videoElement.play();
+});
+
+var videoElement:HTMLVideoElement;
 </script>
 
-hello
+<video autoplay loop muted bind:this={videoElement}>
+    <source src={Bapple}/>
+</video>
